979b9ef7a548d1b1083ae8608ba3617cc2e9e591
Jonathan Olson <jonathan.olson@colorado.edu>
2022-10-27 10:45:08 -0600

    Updating phet-lib with main common code references.

--- js/phet-lib-main.js
+++ js/phet-lib-main.js
@@ -2,15 +2,27 @@
 
 /* eslint-disable default-import-match-filename,bad-sim-text */
 
+// NOTE: Not including mobius, since it requires THREE.js
+
 import axon from '../../axon/js/main.js';
+import bamboo from '../../bamboo/js/main.js';
 import dot from '../../dot/js/main.js';
+import griddle from '../../griddle/js/main.js';
+import joist from '../../joist/js/main.js';
 import kite from '../../kite/js/main.js';
+import mobius from '../../mobius/js/main.js';
+import nitroglycerin from '../../nitroglycerin/js/main.js';
 import phetCore from '../../phet-core/js/main.js';
-import utteranceQueue from '../../utterance-queue/js/main.js';
+import phetcommon from '../../phetcommon/js/main.js';
+import sceneryPhet from '../../scenery-phet/js/main.js';
 import { scenery } from '../../scenery/js/imports.js';
 import sun from '../../sun/js/main.js';
-import sceneryPhet from '../../scenery-phet/js/main.js';
-import phetLib from './main.js'; // eslint-disable-line no-unused-vars
+import tambo from '../../tambo/js/main.js';
+import tandem from '../../tandem/js/main.js';
+import tappi from '../../tappi/js/main.js';
+import twixt from '../../twixt/js/main.js';
+import utteranceQueue from '../../utterance-queue/js/main.js';
+import vegas from '../../vegas/js/main.js';
 
 if ( !window.hasOwnProperty( '_' ) ) {
   throw new Error( 'Underscore/Lodash not found: _' );
@@ -20,11 +32,22 @@ if ( !window.hasOwnProperty( '$' ) ) {
 }
 
 window.axon = axon;
+window.bamboo = bamboo;
 window.dot = dot;
+window.griddle = griddle;
+window.joist = joist;
 window.kite = kite;
+window.mobius = mobius;
+window.nitroglycerin = nitroglycerin;
 window.phetCore = phetCore;
-window.utteranceQueue = utteranceQueue;
+window.phetcommon = phetcommon;
 window.scenery = scenery;
-window.sun = sun;
 window.sceneryPhet = sceneryPhet;
+window.sun = sun;
+window.tambo = tambo;
+window.tandem = tandem;
+window.tappi = tappi;
+window.twixt = twixt;
+window.utteranceQueue = utteranceQueue;
+window.vegas = vegas;
 window.scenery.Utils.polyfillRequestAnimationFrame();
