3316a60e69c50861b3292e037b9a5bf50b641efa
Jonathan Olson <jonathan.olson@colorado.edu>
2023-02-23 16:31:58 -0700

    Moving exclusively to phet.X namespaces, see https://github.com/phetsims/scenery/issues/966

--- js/phet-lib-main.js
+++ js/phet-lib-main.js
@@ -1,28 +1,26 @@
 // Copyright 2022, University of Colorado Boulder
 
-/* eslint-disable default-import-match-filename,bad-sim-text */
-
 // NOTE: Not including mobius, since it requires THREE.js
 
-import axon from '../../axon/js/main.js';
-import bamboo from '../../bamboo/js/main.js';
-import dot from '../../dot/js/main.js';
-import griddle from '../../griddle/js/main.js';
-import joist from '../../joist/js/main.js';
-import kite from '../../kite/js/main.js';
-import mobius from '../../mobius/js/main.js';
-import nitroglycerin from '../../nitroglycerin/js/main.js';
-import phetCore from '../../phet-core/js/main.js';
-import phetcommon from '../../phetcommon/js/main.js';
-import sceneryPhet from '../../scenery-phet/js/main.js';
-import { scenery } from '../../scenery/js/imports.js';
-import sun from '../../sun/js/main.js';
-import tambo from '../../tambo/js/main.js';
-import tandem from '../../tandem/js/main.js';
-import tappi from '../../tappi/js/main.js';
-import twixt from '../../twixt/js/main.js';
-import utteranceQueue from '../../utterance-queue/js/main.js';
-import vegas from '../../vegas/js/main.js';
+import '../../axon/js/main.js';
+import '../../bamboo/js/main.js';
+import '../../dot/js/main.js';
+import '../../griddle/js/main.js';
+import '../../joist/js/main.js';
+import '../../kite/js/main.js';
+import '../../mobius/js/main.js';
+import '../../nitroglycerin/js/main.js';
+import '../../phet-core/js/main.js';
+import '../../phetcommon/js/main.js';
+import '../../scenery-phet/js/main.js';
+import '../../scenery/js/imports.js';
+import '../../sun/js/main.js';
+import '../../tambo/js/main.js';
+import '../../tandem/js/main.js';
+import '../../tappi/js/main.js';
+import '../../twixt/js/main.js';
+import '../../utterance-queue/js/main.js';
+import '../../vegas/js/main.js';
 
 if ( !window.hasOwnProperty( '_' ) ) {
   throw new Error( 'Underscore/Lodash not found: _' );
@@ -31,23 +29,4 @@ if ( !window.hasOwnProperty( '$' ) ) {
   throw new Error( 'jQuery not found: $' );
 }
 
-window.axon = axon;
-window.bamboo = bamboo;
-window.dot = dot;
-window.griddle = griddle;
-window.joist = joist;
-window.kite = kite;
-window.mobius = mobius;
-window.nitroglycerin = nitroglycerin;
-window.phetCore = phetCore;
-window.phetcommon = phetcommon;
-window.scenery = scenery;
-window.sceneryPhet = sceneryPhet;
-window.sun = sun;
-window.tambo = tambo;
-window.tandem = tandem;
-window.tappi = tappi;
-window.twixt = twixt;
-window.utteranceQueue = utteranceQueue;
-window.vegas = vegas;
-window.scenery.Utils.polyfillRequestAnimationFrame();
+phet.scenery.Utils.polyfillRequestAnimationFrame();
