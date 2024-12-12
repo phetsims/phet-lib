789f945c1e7246ea0a304e0444be966cdb7f9b84
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-12-11 17:19:44 -0700

    remove PhetioIDUtils as a preload, https://github.com/phetsims/tandem/issues/316

--- js/minimal-preload.ts
+++ js/minimal-preload.ts
@@ -2,7 +2,6 @@ self.phet = self.phet || {};
 self.phet.chipper = self.phet.chipper || {};
 
 import '../src/sherpa/lib/lodash-4.17.4.js';
-import '../src/tandem/js/PhetioIDUtils.js';
 import '../src/sherpa/lib/linebreak-1.1.0.js';
 import '../src/sherpa/lib/flatqueue-1.2.1.js';
 import '../src/sherpa/lib/he-1.1.1.js';
